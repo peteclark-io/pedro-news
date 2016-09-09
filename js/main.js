3d87c27f809596e4f74a343d5733a7fd35f721c1
Clark, Peter <peter.clark@ft.com>
2016-09-09 13:54:31 +0100

    Changing to jquery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 'use strict'
 
-$.get('/md/20160908.md').then(data => {
+$.get('md/20160908.md').then(data => {
   var md = window.markdownit();
   var result = md.render(data);
 
-  document.getElementById('markdown').innerHTML = result;
+  $('#markdown').html(result);
 });
