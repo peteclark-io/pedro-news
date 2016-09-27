ae12bd9c03c3bd27b900cff8e4685280b7794ae4
Clark, Peter <peter.clark@ft.com>
2016-09-27 17:05:32 +0100

    Adding the latest news

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 'use strict'
 
-$.get('md/20160908.md').then(data => {
+var latest = 'md/20160927.md';
+
+var paths = window.location.hash.split('/');
+console.log(paths, window.location.hash)
+if (paths[1] === 'archive'){
+   latest = 'md/' + paths[2] + '.md';
+   console.log('Updating to archive article.', latest);
+}
+
+$.get(latest).then(data => {
   var md = window.markdownit();
   var result = md.render(data);
 
